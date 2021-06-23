96fabeff9e2d60d92e2ea84beb3d92ec3e63a1a4
NotThorz <reda45696@gmail.com>
2021-06-23 11:25:09 +0000

    added scroll to top on see more products

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import { ProductSet } from './ProductSet'
-import  {Grid}  from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import './Products.css'
 import Product from './Product';
 import { Link } from 'react-router-dom';
 const Products = () => {
     return (
-    <main>
-        <Grid container justify="center" sapcing={4}>
-            {
-                ProductSet.map((product)=>(
-                    <Grid item key={product.id} xd={12} sm={4} md={4} lg={4}>
-                    <Product product={product}/>
-                    </Grid>    
-                ))
-            }
-        </Grid>
-        <div >
-            <Link to="/Products" alt="produit" className="seemore"><p>See More Products</p></Link>
-        </div>
-    </main>
-    
+        <main>
+            <Grid container justify="center" sapcing={4}>
+                {
+                    ProductSet.map((product) => (
+                        <Grid item key={product.id} xd={12} sm={4} md={4} lg={4}>
+                            <Product product={product} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+            <div >
+                <Link to="/Products" alt="produit" className="seemore" onClick={window.onbeforeunload = function () {
+                    window.scrollTo(0, 0);
+                }}
+                ><p>See More Products</p></Link>
+            </div>
+        </main >
+
     )
 }
 
--- src/Pages/ProductSearch.js
+++ src/Pages/ProductSearch.js
@@ -5,17 +5,17 @@ import ProductSearchAll from '../Components/ProductSearchAll/ProductSearchAll'
 import './ProductSearch.css'
 const ProductSearch = () => {
     return (
-        
+
         <div>
-        {/* // header */}
-        <div>
-            <Header className="ProductsHeader" />
-        </div>
-        {/* //search input */}
-        <div className="ProductSearchAll">
-            <ProductSearchAll />
-        </div>
-        
+            {/* // header */}
+            <div>
+                <Header className="ProductsHeader" id="#ProductTop" />
+            </div>
+            {/* //search input */}
+            <div className="ProductSearchAll">
+                <ProductSearchAll />
+            </div>
+
         </div>
     )
 }
